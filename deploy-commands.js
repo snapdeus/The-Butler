01530e1aadacf12a4d289255e2cb7abd00150b2c
snapdeus <snapdeus@gmail.com>
2022-05-30 13:06:13 -0400

    refix config

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,6 +1,6 @@
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { CLIENTID, GUILDID, TOKEN } = require('.//config.json');
+const { CLIENTID, GUILDID, TOKEN } = require('./test/config.json');
 
 const fs = require('fs');
 const path = require('path');
