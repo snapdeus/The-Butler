752bcc0292e187f8a9b2df861826ac7d4c98889a
snapdeus <snapdeus@gmail.com>
2023-04-06 00:02:13 -0400

    added torso json files

--- resources/rpgSeedFiles/testRPGItems.js
+++ resources/rpgSeedFiles/testRPGItems.js
@@ -1,4 +1,20 @@
-const { scrollsMetaInfo } = require('./scrolls')
-const { potionsMetaInfo } = require('./potions')
+const { scrollsMetaInfo, scrolls } = require('./scrolls')
+const { potionsMetaInfo, potions } = require('./potions')
 const { gemsOfPowerMetaInfo } = require('./gemsOfPower')
-console.log(scrollsMetaInfo, '________________________\n\n\n\n________________________', potionsMetaInfo)
+
+console.log(potionsMetaInfo)
+
+const fs = require('fs')
+
+// for (potion of potions) {
+//     potion.Weight = 20
+
+
+// }
+
+// fs.writeFile('./output.json', JSON.stringify(potions), err => {
+//     if (err) {
+//         console.error(err);
+//     }
+//     // file written successfully
+// });
